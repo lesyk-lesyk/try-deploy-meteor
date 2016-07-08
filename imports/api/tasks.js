ee4aad235c66fd1beab1b1959038bfc68eb46bc1
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-07-08 23:15:09 +0300

    add ability to make tasks private and delete only own tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -6,7 +6,12 @@ export const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isServer) {
   Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
   });
 }
 
@@ -27,6 +32,11 @@ Meteor.methods({
 
   'tasks.remove'(taskId) {
     check(taskId, String);
+
+    const task = Tasks.findOne(taskId);
+    if ( task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized or it`s not your task');
+    }
     Tasks.remove(taskId);
   },
 
@@ -34,6 +44,21 @@ Meteor.methods({
     check(taskId, String);
     check(setChecked, Boolean);
 
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.update(taskId, { $set: {checked: setChecked } })
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 });
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
-
+import classnames from 'classnames';
 
 export default class Task extends Component {
   toggleChecked() {
@@ -11,21 +11,35 @@ export default class Task extends Component {
     Meteor.call('tasks.remove', this.props.task._id);
   }
 
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+  }
+
   render() {
 
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
 
     return (
       <li className={taskClassName}>
+        { this.props.task.owner == this.props.currentUserId ? (
         <button className="delete" onClick={this.deleteThisTask.bind(this)}>
           &times;
-        </button>
+        </button> ) : '' }
         <input 
           type="checkbox"
           readOnly
           checked={this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
+
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public' }
+          </button>
+        ) : '' }
 
         <span className="text">
           <strong>{this.props.task.username}</strong>:&nbsp;
@@ -37,5 +51,6 @@ export default class Task extends Component {
 }
 
 Task.propTypes = {
-  task: PropTypes.object.isRequired
+  task: PropTypes.object.isRequired,
+  showPrivateButton: PropTypes.bool.isRequired,
 };
