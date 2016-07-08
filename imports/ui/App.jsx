9faff7c7145154acd19401c0eb8811ff8628b761
okachynskyy <oleksiy.kachynskyy@gmail.com>
2016-07-08 13:39:35 +0300

    add form for adding tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes }  from 'react';
+import ReactDom from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -6,6 +7,18 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const text = this.refs.textInput.value.trim();
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    this.refs.textInput.value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -17,6 +30,14 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input 
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>  
         <ul>
           {this.renderTasks()}
@@ -32,6 +53,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort : { createdAt: -1 } }).fetch(),
   };
 }, App);
